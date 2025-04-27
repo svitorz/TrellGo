e750c1c8e084931f15b73a869342934d3af66b53
svitorz <vitor.fabio.castro@gmail.com>
2025-04-27 01:20:37 -0300

    feat: loading env vars

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConnection = ""
+	Port             = 0
+)
+
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 9000
+	}
+
+	StringConnection = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+	)
+}
